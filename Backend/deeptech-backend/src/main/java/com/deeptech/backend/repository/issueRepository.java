c8e13737ad6a9b3856986b94b959b0f6ababf127
Shubh Pandey <shubhpandey2985@gmail.com>
2026-09-24 13:16:02 +0530

    New

--- Backend/deeptech-backend/src/main/java/com/deeptech/backend/repository/issueRepository.java
+++ Backend/deeptech-backend/src/main/java/com/deeptech/backend/repository/issueRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.List;
-import java.util.Map;
 
 public interface issueRepository extends JpaRepository<issue, Long> {
 
